ad29652e25e2286df86109b43da45cc96c06edb4
clipstoedit <133930404+clipstoedit@users.noreply.github.com>
2024-04-04 00:42:31 +0100

    Update index.tsx

--- index.tsx
+++ index.tsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import Link from 'next/link'; // Import Link from Next.js
-import styles from '../styles/styles.module.css'; // Import CSS styles
+import styles from './styles/styles.module.css'; // Import CSS styles
 
 const Leaderboard = () => {
     const [leaderboardData, setLeaderboardData] = useState([]);
